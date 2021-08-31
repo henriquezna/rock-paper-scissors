d961d8967f482fb52e2117fd826b478ebeda99a8
Nathan <henriquezna@gmail.com>
2021-08-30 20:59:08 -0500

    Created UI elements

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(){
     const randNum = Math.ceil(Math.random()*3);
     switch (randNum){
@@ -22,7 +25,40 @@ function playerPlay(){
     return `${sign[0].toUpperCase()}${sign.substr(1)}`;
 }
 
-function round(computerSelection, playerSelection){
+// function round(computerSelection, playerSelection){
+//     let winner = "";
+//     if(computerSelection === playerSelection){
+//         winner = "It's a tie!"
+//     }else if (computerSelection === "Rock"){
+//         playerSelection === "Scissors" ? winner = "You Lose" : winner = "You Win!";
+//     }else if (computerSelection === "Paper"){
+//         playerSelection === "Scissors" ? winner = "You Win!" : winner = "You Lose";
+//     }else{
+//         playerSelection === "Rock" ? winner = "You Win!" : winner = "You Lose";
+//     }
+
+//     console.log("Computer: " + computerSelection + " You: " + playerSelection);
+//     return winner;
+// }
+
+// function game(){
+    // for(i = 0; i < 5; i++){
+    //     console.log("ROUND " + (i+1));
+    //     console.log(round(computerPlay(), playerPlay()));
+    // }
+// }
+
+function reset(){
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function playRound(playerSelection){
+    const computerSelection = computerPlay();
+
+    const div = document.querySelector("#results");
+    const divScore = document.querySelector("#score");
+
     let winner = "";
     if(computerSelection === playerSelection){
         winner = "It's a tie!"
@@ -35,15 +71,28 @@ function round(computerSelection, playerSelection){
     }
 
     console.log("Computer: " + computerSelection + " You: " + playerSelection);
-    return winner;
-}
+    div.textContent = "Computer: " + computerSelection + " You: " + playerSelection + " " + winner;
 
-function game(){
-    for(i = 0; i < 5; i++){
-        console.log("ROUND " + (i+1));
-        console.log(round(computerPlay(), playerPlay()));
+    if(winner === "You Win!"){
+        playerScore++;
+    }else{
+        computerScore++;
     }
+
+    if(playerScore === 5){
+        div.textContent = "YOU WON!";
+        divScore.textContent= "Player: " + playerScore + " Computer: " + computerScore;
+        reset();
+    }else if(computerScore === 5){
+        div.textContent = "COMPUTER WON!";
+        divScore.textContent= "Player: " + playerScore + " Computer: " + computerScore;
+        reset();
+    }else{
+        divScore.textContent= "Player: " + playerScore + " Computer: " + computerScore;
+    }
+
+    // return winner;
 }
 
-console.log("Welcome!");
-game();
+// console.log("Welcome!");
+// game();
